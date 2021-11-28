e7d231ad64a12a0ea2a290fdfce643d823ff7784
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-11-28 13:39:39 -0500

    a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -52,9 +52,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6bed447d4d729c40ef3f92d1a60a3d29",
-"/": "6bed447d4d729c40ef3f92d1a60a3d29",
-"main.dart.js": "c1e1017e5421f86ed13fdbc3f2f48e07",
+"index.html": "ef90a73e1c7aa7c9cdcc9740cedca431",
+"/": "ef90a73e1c7aa7c9cdcc9740cedca431",
+"main.dart.js": "026b47852ad6248e834b0f5b4a9afd26",
 "manifest.json": "411f3a19b5b75b69d104fe680c37c26c",
 "version.json": "02bbd8a7087147d4ad4f4421298c52ec"
 };
